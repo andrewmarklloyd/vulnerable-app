c64d98cf2eaad856892a75dd94a06dde76a1fde9
Andrew Lloyd <andrew85.lloyd@gmail.com>
2020-02-25 07:47:40 -0800

    Add index and subtitles

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
 app.get('/query', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/query.html'))
 });
